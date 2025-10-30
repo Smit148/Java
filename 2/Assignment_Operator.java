09753d919a10bb77e3a5354eddd9b6b1ef324730
Smit Kadam <smitkadam148@gmail.com>
2025-10-30 19:09:34 +0530

    assign

---
+++ 2/Assignment_Operator.java
@@ -0,0 +1,22 @@
+public class Assignment_Operator 
+{
+    public static void main(String[] args) 
+    {
+        int x=5;
+        System.out.println("Assignment: "+x);
+
+        x+=3;
+        System.out.println("Addition Assignment: "+ x);
+
+        x-=2;
+        System.out.println("Substraction Assignment: "+ x);
+        
+
+        x*=4;
+        System.out.println("Multiplication Assignment: "+ x);
+
+        x/=2;
+        System.out.println("Division Assignment: "+ x);
+
+    }
+}
