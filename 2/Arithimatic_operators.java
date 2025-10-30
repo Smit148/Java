c0cf877273d3c48e3de40d4f6cf94cc1ff91ec2b
Smit Kadam <smitkadam148@gmail.com>
2025-10-30 19:09:25 +0530

    arithmatic

---
+++ 2/Arithimatic_operators.java
@@ -0,0 +1,23 @@
+class Arithimatic_operators 
+{
+    public static void main(String [] args)
+    {
+        System.out.println(" ============== arthimatic Operators ============= ");
+
+        int add = 5+2;
+        System.out.println("Addition is: " + add);
+
+        int sub = 5-2;
+        System.out.println("Addition is: " + sub);
+
+        int mul = 5*2;
+        System.out.println("Addition is: " + mul);
+
+        int div = 5/2;
+        System.out.println("Addition is: " + div);
+
+        int mod = 5%2;
+        System.out.println("Addition is: " + mod);
+
+    }
+}
