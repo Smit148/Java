21054a9ebd4692d43043143de59aa1190b984a03
Smit Kadam <smitkadam148@gmail.com>
2025-10-30 19:08:54 +0530

    comparision

---
+++ 2/Comparision.java
@@ -0,0 +1,23 @@
+class Comparision 
+{
+    public static void main(String[] args)
+    {
+        boolean Equal = (5 == 5);
+        System.out.println("Equal to: " + Equal);
+        
+        boolean Notequal = (10 != 5);
+        System.out.println("Equal to: " + Notequal);
+        
+        boolean Great = (8 > 3);
+        System.out.println("Equal to: " + Great);
+        
+        boolean Lesser = (4 < 9);
+        System.out.println("Equal to: " + Lesser);
+        
+        boolean GreatEqual = (7 >= 6);
+        System.out.println("Equal to: " + GreatEqual);
+        
+        boolean LesserEqual = (2 <= 3);
+        System.out.println("Equal to: " + LesserEqual);
+    }    
+}
