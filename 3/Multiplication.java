5f5bb804d0fe4681345cb73577a7d7b49d837850
Smit Kadam <smitkadam148@gmail.com>
2025-11-05 20:54:46 +0530

    multiplication

---
+++ 3/Multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication{
+    public static void main(String[] args)
+    {
+        int n =5;
+        for (int i=1;i<11;i++)
+        {
+            int m= i*n;
+            System.out.println(n+" x "+i+" = "+m);
+        }
+    }
+}
