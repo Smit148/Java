dbcff3a67a92be97ba21c545126b990b76883206
Smit Kadam <smitkadam148@gmail.com>
2025-11-05 20:55:59 +0530

    object

---
+++ 3/Object.java
@@ -0,0 +1,15 @@
+class Aven 
+{
+    void mission()
+    {
+        System.out.println("Mission started...");
+    }    
+}
+
+public class Object
+{
+    public static void main(String[] args) {
+        Aven a = new Aven();
+        a.mission();
+    }
+}
