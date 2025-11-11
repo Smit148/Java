fc74c210486ff2d828121023f632d1aa32521ae1
Smit Kadam <smitkadam148@gmail.com>
2025-11-11 17:25:06 +0530

    array

---
+++ 5/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+    public static void main(String[] args)
+    {
+        int[] num;
+        num = new int[5];  // declaration
+
+        num[0] = 100;
+        num[1] = 200;
+        num[2] = 300;
+        num[3] = 400;
+        num[4] = 500;
+        
+        System.out.println("Accessing through index: ");
+        System.out.println(num[2]);
+        
+    }
+    
+}
