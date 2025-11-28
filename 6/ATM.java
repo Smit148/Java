2529a1e5077b66ed746d90c298a7a9d624f4d511
Smit Kadam <smitkadam148@gmail.com>
2025-11-28 18:25:39 +0530

    Create ATM.java

---
+++ 6/ATM.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ATM {
+    
+    Scanner sc = new Scanner(System.in);
+
+    int balance = 0;
+
+    int withdraw;
+
+    int deposite;
+
+    int acc;
+
+}
