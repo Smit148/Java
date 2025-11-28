75125ace0f5421e77967ea7417c728176577d2e8
Smit Kadam <smitkadam148@gmail.com>
2025-11-28 18:12:59 +0530

    Create Shape.java

---
+++ 11/Shape.java
@@ -0,0 +1,11 @@
+
+abstract class Shape 
+{
+    abstract float area();
+    abstract float perimeter();
+
+    void design()
+    {
+        System.out.println("Which Design Do You Like");        
+    }
+}
