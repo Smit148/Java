9ffb470389b1c8de3c0c0127bb872979c2d2f1e7
Smit Kadam <smitkadam148@gmail.com>
2025-12-17 17:43:55 +0530

    Create tempCodeRunnerFile.java

---
+++ 15/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+
+        // System.out.println("Length Of Row "+neo.length);
+        // System.out.println("Length Of Column "+neo[0].length);
+
+        // System.out.println("Accessing 2D Array Through Index: ");
+        // System.out.println("Element of index [1][3] "+neo[1][3]);
